98df54e3793a8ea3b5ff63fe2b2d5d22006db79f
me333e <alikate458@gmail.com>
2025-12-01 01:12:58 -0800

    added the disabled button feature

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -25,12 +25,10 @@ export class Start extends Phaser.Scene {
         this.load.image('basicButton', 'assets/Fireball.png');
         this.load.image('basicButtonDisabled', 'assets/FireballDisabled.png');
 
-        this.load.spritesheet('basicShooter', 'assets/NovicePyromancer.png', 
-            {
-                frameWidth: 16,
-                frameHeight: 16,
-            }
-        );
+        this.load.spritesheet('basicShooter', 'assets/NovicePyromancer.png', {
+            frameWidth: 16,
+            frameHeight: 16,
+        });
 
     }
 
@@ -52,11 +50,11 @@ export class Start extends Phaser.Scene {
         this.timer = 0;
 
         this.anims.create({
-                key: "idle",
-                frames: this.anims.generateFrameNumbers('basicShooter', {start: 0, end: 4}),
-                frameRate: 6,
-                repeat: -1
-            });
+            key: "idle",
+            frames: this.anims.generateFrameNumbers('basicShooter', {start: 0, end: 4}),
+            frameRate: 6,
+            repeat: -1
+        });
 
         //will need to make more buttons for every tower area, maybe there's a way to make them all at once
         this.buttons = this.add.group("buttons");
@@ -75,13 +73,14 @@ export class Start extends Phaser.Scene {
         this.basicButton.on('pointerdown', () => { 
             this.buildBasic();
         });
-        this.basicButtonClicked = false;
 
-        this.basicButtonDisabled = this.add.image(360, 90, 'basicButtonDisabled').setOrigin(0);
+        this.basicButtonDisabled = this.add.image(460, 123, 'basicButtonDisabled').setOrigin(0);
         this.basicButtonDisabled.setVisible(false);
+        this.basicAfford = false;
 
         this.buttons.add(this.buildButton);
         this.buttons.add(this.basicButton);
+        this.buttons.add(this.basicButtonDisabled);
 
     }
 
@@ -89,37 +88,61 @@ export class Start extends Phaser.Scene {
         this.timer += delta;
         while (this.timer > 1000) {
             this.currency += 1;
+            this.events.emit('updateCurrency', this.currency);
             this.timer -= 1000;
+        }
+
+        if (this.currency > 5) {
+            this.basicAfford = true;
+        }
+        else {
+            this.basicAfford = false;
         }
 
         this.events.emit('updateHP', this.playerhp);    //use when you want to update hp
         this.events.emit('updateWave', this.wave);      //use when you want to update wave number
-        this.events.emit('updateCurrency', this.currency);      //use when you want to update wave number
 
     }
 
     buildTower() {
         if (!this.buildButtonClicked) {
-                this.buildButtonClicked = true;         //add conditions to see if player has enough currency to build tower
-                this.basicButton.setVisible(true);      //otherwise display the disabled version of the button
+            this.buildButtonClicked = true;         //add conditions to see if player has enough currency to build tower
+            if (this.basicAfford == true) {         //the basicAfford is not updated if the build button is left opened, should fix this
+                this.basicButtonDisabled.setVisible(false);
+                this.basicButton.setVisible(true);
                 this.basicButton.enable = true;
-        }
-        else {
-                this.buildButtonClicked = false;
+            }
+            else {
+                this.basicButtonDisabled.setVisible(true);
                 this.basicButton.setVisible(false);
                 this.basicButton.enable = false;
+            }
+        }
+        else {
+            this.buildButtonClicked = false;
+            this.basicButtonDisabled.setVisible(false);
+            this.basicButton.setVisible(false);
+            this.basicButton.enable = false;
         }
     }
 
     buildBasic() {
-            this.buildButtonClicked = false;
-            this.buttons.setVisible(false);
-            this.buttons.enable = false;
+        this.buildButtonClicked = false;
+        this.buttons.setVisible(false);
+        this.buttons.enable = false;
+        this.currency -= 5;
 
-            const basic = new BasicShooter({scene: this, x: 490, y: 145,});
-            basic.setDepth(1);
+        const basic = new BasicShooter({scene: this, x: 490, y: 145,});
+        basic.setDepth(1);
+        basic.setInteractive();
+        basic.on('pointerdown', () => { 
+            this.upgrades();
+        });
     }
 
+    upgrades() {
+        //add the upgrade option and delete option which will give the build button options back and refund some currency
+    }
 
     checkEndGame()      //prob should add if statment to check if player hp has hit 0 yet fo lose end game, we need to also make a win one
     {
